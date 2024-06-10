0d98763d1f5968e071327afb218d3c56bb8a7677
Henry Denny <henry.denny12@gmail.com>
2024-06-10 10:24:12 +0100

    Fix dependency issue.

--- trace-archiver-tdengine/src/main.rs
+++ trace-archiver-tdengine/src/main.rs
@@ -8,6 +8,7 @@ use rdkafka::{
     consumer::{stream_consumer::StreamConsumer, CommitMode, Consumer},
     message::Message,
 };
+use supermusr_common::tracer::OtelTracer;
 use supermusr_common::{conditional_init_tracer, tracer::OptionalHeaderTracerExt};
 use supermusr_streaming_types::dat2_digitizer_analog_trace_v2_generated::{
     digitizer_analog_trace_message_buffer_has_identifier, root_as_digitizer_analog_trace_message,
